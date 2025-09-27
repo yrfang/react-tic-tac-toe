c313060a9aa84e3b20b1bb78f2b826410367c8a7
Yingru Fang <fyr1116@gmail.com>
2025-09-27 12:35:44 -0700

    Add tic-tac-toe  game history

--- src/pages/TicTacToe.jsx
+++ src/pages/TicTacToe.jsx
@@ -4,33 +4,72 @@ function Square({ value, onSquareClick }) {
   return <button onClick={onSquareClick}>{value}</button>;
 }
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  function handleClick(i) {
-    if (squares[i]) return;
-    const nextSquares = squares.slice();
-    nextSquares[i] = xIsNext ? "X" : "O";
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-  }
-
-  const winner = calculateWinner(squares);
-  const status = winner
-    ? `Winner: ${winner}`
-    : `Next player: ${xIsNext ? "X" : "O"}`;
-
+function Board({ squares, onSquareClick }) {
   function renderSquare(i, key) {
-    return <Square key={key} value={squares[i]} onSquareClick={() => handleClick(i)} />;
+    return (
+      <Square
+        key={key}
+        value={squares[i]}
+        onSquareClick={() => onSquareClick(i)}
+      />
+    );
   }
 
   return (
     <div>
+      <div>{[0, 1, 2].map(num => renderSquare(num, num))}</div>
+      <div>{[3, 4, 5].map(num => renderSquare(num, num))}</div>
+      <div>{[6, 7, 8].map(num => renderSquare(num, num))}</div>
+    </div>
+  );
+}
+
+export default function TicTacToe() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function handleClick(i) {
+    if (currentSquares[i] || calculateWinner(currentSquares)) return;
+
+    const nextSquares = currentSquares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+    handlePlay(nextSquares);
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+  }
+
+  const winner = calculateWinner(currentSquares);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
+
+  return (
+    <div>
+      <h1>Tic Tac Toe</h1>
       <div>{status}</div>
-      <div>{[0,1,2].map(num => renderSquare(num, num))}</div>
-      <div>{[3,4,5].map(num => renderSquare(num, num))}</div>
-      <div>{[6,7,8].map(num => renderSquare(num, num))}</div>
+      <Board squares={currentSquares} onSquareClick={handleClick} />
+      
+      {/* History moves list */}
+      <ol>
+        {history.map((_, move) => {
+          const description = move === 0 ? "Go to game start" : `Go to move #${move}`;
+          return (
+            <li key={move}>
+              <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 }
@@ -47,13 +86,4 @@ function calculateWinner(squares) {
     }
   }
   return null;
-}
-
-export default function TicTacToe() {
-  return (
-    <div>
-      <h1>Tic Tac Toe</h1>
-      <Board />
-    </div>
-  );
 }
